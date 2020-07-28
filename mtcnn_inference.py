9daee7bd7f6dbcb2f7b0cc221f220da964f63e88
chaffee.chen <chaffee.chen@ucloud.cn>
2020-07-28 17:06:21 +0800

    bgfx

--- mtcnn_inference.py
+++ mtcnn_inference.py
@@ -1,7 +1,7 @@
 import os,sys
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
+# curPath = os.path.abspath(os.path.dirname(__file__))
+# rootPath = os.path.split(curPath)[0]
+# sys.path.append(rootPath)
 
 pj = os.path.join
 
